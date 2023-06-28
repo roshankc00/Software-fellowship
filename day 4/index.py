d6f95da2b66d4cca918b723ab7e90d5686cb0071
Roshan karki <rohitkc8848@gmail.com>
2023-06-28 19:08:48 +0545

    feat:day 4

---
+++ day 4/index.py
@@ -0,0 +1,17 @@
+import numpy as np
+x=[34,5,3,7,84,3]
+np.array(x)
+
+
+
+y=(np.random.rand(100)).astype(int)
+x=np.random.rand(100)*10
+x.astype(int)
+# print(x**2)
+print(x)
+# print(y)
+
+
+
+np.char.capitalize(["python", "numpy"])
+np.char.add(["num", "doc"], ["py", "umentation"])
